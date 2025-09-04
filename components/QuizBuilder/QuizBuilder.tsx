cabe8cd21b893bc5009850b9e574765c616bd625
Olga Kalacheva <kalach2256954@gmail.com>
2025-09-04 18:52:14 +0400

    add the right sidebar

--- components/QuizBuilder/QuizBuilder.tsx
+++ components/QuizBuilder/QuizBuilder.tsx
@@ -4,16 +4,16 @@ import { DndContext } from "@dnd-kit/core";
 import { Canvas } from "./Canvas";
 import { LeftSidebar } from "./LeftSidebar";
 import { DragOverlayWrapper } from "./DragOverlayWrapper";
+import { RightSidebar } from "./RightSidebar";
 
 export const QuizBuilder = () => {
   return (
     <DndContext>
       <main className="flex flex-col w-full mt-24">
-        <h2>Buid your quiz</h2>
-
         <div className="flex w-full min-h-screen p-10 justify-around bg-accent bg-[url(/paper.svg)]">
           <LeftSidebar />
           <Canvas />
+          <RightSidebar />
         </div>
       </main>
       <DragOverlayWrapper />
---
+++ components/QuizBuilder/RightSidebar.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useElementContext } from "@/app/hooks/useElementContext";
+import { QuizElements } from "./QuizElements";
+
+export const RightSidebar = () => {
+  const { selectedElement } = useElementContext();
+
+  if (!selectedElement) {
+    return (
+      <aside className="w-[480px] max-w-[400px] flex items-center flex-col  flex-grow gap-2 border border-gray-500 rounded-xl p-4 bg-background overflow-y-auto">
+        <p className="text-3xl text-muted-foreground font-bold">Edit Panel</p>
+        <p>No element selected</p>
+      </aside>
+    );
+  }
+
+  const PropertiesComponent = QuizElements[selectedElement.type].quizComponent;
+
+  return (
+    <aside className="w-[480px] max-w-[400px] flex items-center flex-col  flex-grow gap-2 border border-gray-500 rounded-xl p-4 bg-background overflow-y-auto">
+      <p className="text-3xl text-muted-foreground font-bold">Edit Panel</p>
+      <PropertiesComponent />
+    </aside>
+  );
+};
