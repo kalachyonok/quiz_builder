bcfdf777c19ec3605fa8bc0116a0aa3359b2d3f2
Olga Kalachova <kalach2256954@gmail.com>
2025-09-11 00:02:54 +0400

    cleanup edit panel

--- app/(without-header)/page.tsx
+++ app/(without-header)/page.tsx
@@ -6,18 +6,21 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useQuizContext } from "../../hooks/useQuizContext";
 import { useElementContext } from "../../hooks/useElementContext";
+import { useEffect } from "react";
 
 export default function HomePage() {
   const { quizzes } = useQuizContext();
   const { setSelectedElement } = useElementContext();
+
+  useEffect(() => {
+    setSelectedElement(null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container>
       <div className="container mx-auto">
-        <Button
-          asChild
-          className="bg-red-700 cursor-pointer mb-8"
-          onClick={() => setSelectedElement(null)}
-        >
+        <Button asChild className="bg-red-700 cursor-pointer mb-8">
           <Link href="/quiz/edit/new">Create new Quiz</Link>
         </Button>
         <DataTable data={quizzes} />
--- app/quiz/edit/[id]/page.tsx
+++ app/quiz/edit/[id]/page.tsx
@@ -3,11 +3,20 @@ import { Container } from "@/components/container";
 import { QuizBuilder } from "@/components/QuizBuilder/QuizBuilder";
 import { useParams } from "next/navigation";
 import { useQuizContext } from "@/hooks/useQuizContext";
+import { useElementContext } from "@/hooks/useElementContext";
 import { Header } from "@/components/header";
+import { useEffect } from "react";
 
 export default function IndividualQuizPage() {
   const { id } = useParams();
   const { quizzes } = useQuizContext();
+  const { setSelectedElement } = useElementContext();
+
+  useEffect(() => {
+    setSelectedElement(null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const quiz = quizzes.find((quiz) => quiz.id === Number(id));
   return (
     <Container>
