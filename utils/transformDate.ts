84221231ff21f2b177c4ddfbe218e71459a14d07
Olga Kalachova <kalach2256954@gmail.com>
2025-09-10 12:35:18 +0400

    change date format

--- utils/transformDate.ts
+++ utils/transformDate.ts
@@ -1,8 +1,14 @@
 export const transformDateFromISO = (inputDate: string): string => {
   const date = new Date(inputDate);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${day}.${month}.${year}`;
+  
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true
+  };
+  
+  return date.toLocaleDateString('en-US', options);
 };
