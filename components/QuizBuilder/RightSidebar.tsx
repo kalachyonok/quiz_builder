6dafd77ac94e764173b49f425fb8f8224e32eb8e
Olga Kalachova <kalach2256954@gmail.com>
2025-09-11 18:50:47 +0400

    improve RightSidebar

--- components/QuizBuilder/RightSidebar.tsx
+++ components/QuizBuilder/RightSidebar.tsx
@@ -3,28 +3,20 @@ import { useElementContext } from "@/hooks/useElementContext";
 import { QuizElements } from "./QuizElements";
 
 export const RightSidebar: React.FC = () => {
-  const { selectedElement, elements } = useElementContext();
+  const { selectedElement } = useElementContext();
 
-  if (!selectedElement) {
-    return (
-      <aside className="w-[480px] max-w-[400px] flex items-center flex-col  flex-grow gap-2 border border-gray-500 rounded-xl p-4 bg-background overflow-y-auto">
-        <p className="text-3xl text-muted-foreground font-bold">Edit Panel</p>
-        <p>No element selected</p>
-      </aside>
-    );
-  }
-
-  const PropertiesComponent =
-    QuizElements[selectedElement.type].propertiesComponent;
+  const PropertiesComponent = selectedElement
+    ? QuizElements[selectedElement.type].propertiesComponent
+    : null;
 
   return (
-    <aside className="w-[480px] max-w-[400px] flex items-center flex-col  flex-grow gap-2 border border-gray-500 rounded-xl p-4 bg-background overflow-y-auto">
+    <aside className="w-[480px] max-w-[400px] flex items-center flex-col flex-grow gap-2 border border-gray-500 rounded-xl p-4 bg-background overflow-y-auto">
       <p className="text-3xl text-muted-foreground font-bold">Edit Panel</p>
 
-      {elements.length === 0 ? (
-        <p>No element selected</p>
-      ) : (
+      {selectedElement && PropertiesComponent ? (
         <PropertiesComponent elementInstance={selectedElement} />
+      ) : (
+        <p>No element selected</p>
       )}
     </aside>
   );
